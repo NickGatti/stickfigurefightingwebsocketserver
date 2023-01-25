043aaa554fbd2a63449990e27200a91c3850687c
Nick Gatti <nick.gatti@gmail.com>
2023-01-24 23:14:45 -0700

    other player moves independantly

--- server/server.js
+++ server/server.js
@@ -3,10 +3,42 @@ const wss = new WebSocket.Server({ port: 7071 });
 
 const clients = new Map();
 
+const players = {
+  one: {
+    connected: false,
+    id: null
+  },
+  two: {
+    connected: false,
+    id: null
+  }
+}
+
 wss.on('connection', (ws) => {
     const id = uuidv4();
     const color = Math.floor(Math.random() * 360);
-    const metadata = { id, color };
+
+    if (!players.one.connected) {
+      players.one.connected = true;
+      players.one.id = { 
+        number: 1,
+        uuid: id
+      };
+      console.log('P1 Joined')
+    } else if (!players.two.connected) {
+      players.two.connected = true;
+      players.two.id = {
+        number: 2,
+        uuid: id
+      };
+      console.log('P2 Joined');
+    } else {
+      console.log('Too many players.');
+    }
+
+    const playerNumber = id === players.one.id.uuid ? 1 : 2
+
+    const metadata = { id, color, playerNumber };
 
     clients.set(ws, metadata);
 
@@ -14,12 +46,19 @@ wss.on('connection', (ws) => {
         const message = JSON.parse(messageAsString);
         const metadata = clients.get(ws);
 
-        console.log(message)
+        console.log('meta data', metadata)
   
-        message.sender = metadata.id;
-        message.color = metadata.color;
+        
+        // message.sender = metadata.id;
+        // message.color = metadata.color;
+        // message.player = metadata.id === players.one.id ?? players.one.id
+        // message.player = metadata.id === players.two.id ?? players.two.id
 
-        const outbound = JSON.stringify(message);
+        console.log('MSG:', message)
+
+        console.log(players)
+
+        const outbound = JSON.stringify({message, playerNumber});
 
         [...clients.keys()].forEach((client) => {
           client.send(outbound);
